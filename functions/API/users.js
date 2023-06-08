c46d1ec36a7030e7a8f515dc29967d3650bfc827
leticia.vargas <le.mvargas@gmail.com>
2023-06-08 20:09:44 -0300

    Cleaning up

--- functions/API/users.js
+++ functions/API/users.js
@@ -4,7 +4,6 @@ import { collection, getDocs, query, where, doc, setDoc } from 'firebase/firesto
 
 import { validateCreateUserData } from '../util/validators.js';
 
-
 export const createUser = async (request, response) => {
   const password = request.body.password
   const newUser = {
@@ -22,8 +21,6 @@ export const createUser = async (request, response) => {
     }]
   };
 
-  console.log("usuário  >>>", newUser);
-
   const { valid, errors } = validateCreateUserData(newUser);
 
   if (!valid) return response.status(400).json(errors);
@@ -37,7 +34,6 @@ export const createUser = async (request, response) => {
     console.log("CADASTRAR O USUÁRIO MONGÃO");
     try {
       const credential = await createUserWithEmailAndPassword(auth, newUser.email, password);
-      console.log("credencial >>>", credential.user.toJSON().stsTokenManager.accessToken);
       const userUid = credential?.user.uid;
       const token = credential?.user.toJSON().stsTokenManager.accessToken;
       await setDoc(doc(firestore, "users", userUid), {
@@ -46,7 +42,6 @@ export const createUser = async (request, response) => {
       return response.status(201).json({ token });
       
     } catch (error) {
-      console.log({error})
       if (error.code === 'auth/email-already-in-use') {
         return response.status(400).json({ email: 'Email already in use' });
       } else {
@@ -55,52 +50,4 @@ export const createUser = async (request, response) => {
     }
 
   }
-  
- 
-/*db
-    .doc(`/users/${newUser.email}`)
-    .get()
-    .then((doc) => {
-        if (doc.exists) {
-            return response.status(400).json({ username: 'this username is already taken' });
-        } else {
-            return firebase
-                    .auth()
-                    .createUserWithEmailAndPassword(
-                        newUser.email, 
-                        newUser.password
-                );
-        }
-    })
-    .then((data) => {
-        userId = data.user.uid;
-        return data.user.getIdToken();
-    })
-    .then((idtoken) => {
-        token = idtoken;
-        const userCredentials = {
-            firstName: newUser.firstName,
-            lastName: newUser.lastName,
-            username: newUser.username,
-            phoneNumber: newUser.phoneNumber,
-            country: newUser.country,
-            email: newUser.email,
-            createdAt: new Date().toISOString(),
-            userId
-        };
-        return db
-                .doc(`/users/${newUser.username}`)
-                .set(userCredentials);
-    })
-    .then(()=>{
-        return response.status(201).json({ token });
-    })
-    .catch((err) => {
-  console.error(err);
-  if (err.code === 'auth/email-already-in-use') {
-    return response.status(400).json({ email: 'Email already in use' });
-  } else {
-    return response.status(500).json({ general: 'Something went wrong, please try again' });
-  }
-});*/
 };
