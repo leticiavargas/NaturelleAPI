b0d08f34d5dfade559254fb61cd026862e0826a9
leticia.vargas <le.mvargas@gmail.com>
2023-06-08 23:00:34 -0300

    add average rating and count of reviews on products

--- functions/API/products.js
+++ functions/API/products.js
@@ -1,23 +1,44 @@
 import { collection, getDocs, doc, getDoc } from 'firebase/firestore';
 import { firestore } from '../util/firebase.js';
+import { getCategory } from './categories.js';
+import { averageRate } from './reviews.js';
 
 export const getAllProducts = async (request, response) => {
 
   let products = [];
   const querySnapshot = await getDocs(collection(firestore, "products"));
-  
-  if(querySnapshot.size > 0) {
-    querySnapshot.forEach((doc) => {
+
+  if (querySnapshot.size > 0) {
+    let error = false;
+
+    for (let doc of querySnapshot.docs) {
+      const product = doc.data();
+      const category = await getCategory(product.categoryId);
+      if (!category) {
+        error = true;
+        break;
+      }
+
+      const average = await averageRate(doc.id);
+
       products.push({
         id: doc.id,
-        ...doc.data()
+        category: category.name,
+        avgRate: average?.avgRate ?? 0,
+        reviewsCount: average?.reviewsCount ?? 0,
+        ...product
       })
-    })
-    response.status(200).json(products);
+    }
+
+    if (error) {
+      response.status(417).json({ message: "Oopsie-daisy! It appears that the category registration went a little off course."});
+    } else {
+      response.status(200).json(products);
+    }
   } else {
-    response.status(404).json({ message: "Whoopsie-daisy! It appears that the list of products decided to play hide-and-seek with us."})
+    response.status(404).json({ message: "Whoopsie-daisy! It appears that the list of products decided to play hide-and-seek with us." })
   }
-  
+
 }
 
 export const getProduct = async (request, response) => {
@@ -25,9 +46,25 @@ export const getProduct = async (request, response) => {
 
   const docSnap = await getDoc(doc(firestore, "products", productId));
 
-  if(docSnap.exists()) {
-    response.status(200).json(docSnap.data());
+  if (docSnap.exists()) {
+    const product = docSnap.data();
+    const category = await getCategory(product.categoryId);
+   
+    if (!category) {
+      response.status(417).json({ message: "Oopsie-daisy! It appears that the category registration went a little off course."});
+    } else {
+      const average = await averageRate(productId);
+      delete product.categoryId;
+  
+      response.status(200).json({
+        ...product,
+        category: category?.name,
+        avgRate: average?.avgRate ?? 0,
+        reviewsCount: average?.reviewsCount ?? 0,
+      });
+    }
+
   } else {
-    response.status(404).json({message : "Oops! Looks like your product went on a little adventure without us."})
+    response.status(404).json({ message: "Oops! Looks like your product went on a little adventure without us." })
   }
 }
--- functions/API/reviews.js
+++ functions/API/reviews.js
@@ -1,11 +1,12 @@
-import { collection, getDocs, doc, getDoc, query, where } from 'firebase/firestore';
+import { collection, getDocs, doc, getDoc, query, where, orderBy } from 'firebase/firestore';
 import { firestore } from '../util/firebase.js';
 
 export const getAllReviewsByProduct = async (request, response) => {
   const productId = request.params.id;
 
   let reviews = [];
-  const querySnapshot = await getDocs(query(collection(firestore, "reviews"), where("productId", "==", productId)));
+  const q = query(collection(firestore, "reviews"), where("productId", "==", productId), orderBy("createdAt", "desc"))
+  const querySnapshot = await getDocs(q);
 
   if(querySnapshot.size > 0) {
     querySnapshot.forEach((doc) => {
@@ -30,5 +31,26 @@ export const getReview = async (request, response) => {
     response.status(200).json(docSnap.data());
   } else {
     response.status(404).json({message : "Uh-oh! It looks like that particular review took a little vacation."})
+  }
+}
+
+export const averageRate = async(productId) => {
+  
+  const querySnapshot = await getDocs(query(collection(firestore, "reviews"), where("productId", "==", productId)));
+  const reviewsCount = querySnapshot.size;
+  let rateSum = 0;
+  let avgRate = 0;
+
+  if(reviewsCount > 0) {
+    querySnapshot.forEach((doc) => {
+      rateSum+= parseInt(doc.data().rate);
+    })
+    avgRate = rateSum / reviewsCount;
+  }
+
+
+  return {
+    avgRate,
+    reviewsCount
   }
 }
